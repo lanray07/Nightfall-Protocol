1e3f12addfc7832bb7ffcc869c49607d29ff2716
lanray07 <lanraybanks@gmail.com>
2026-05-24 21:07:31 +0100

    Use App Store premium pass product id

--- NightfallProtocol/StoreKit/StoreService.swift
+++ NightfallProtocol/StoreKit/StoreService.swift
@@ -13,7 +13,7 @@ typealias ProductPurchaseHandler = @MainActor (Product) async throws -> Product.
 @Observable
 final class StoreService {
     static let starterPackID = "com.nightfallprotocol.cosmetic.starter"
-    static let premiumPassID = "com.nightfallprotocol.subscription.premium"
+    static let premiumPassID = "com.nightfallprotocol.pass.premium"
     static let nightmareSkinPackID = "com.nightfallprotocol.cosmetic.nightmare"
 
     private(set) var products: [Product] = []
